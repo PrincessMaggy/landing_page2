1a78c37de2707ee7e1e0fa28ccf3c1526f4bdf50
PrincessMaggy <edoziemagdalene@gmail.com>
2023-03-18 08:42:17 +0100

    ...

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,10 @@
 import Carousel from 'react-bootstrap/Carousel';
 import hero1 from '../img/hero1.jpg';
+import hero2 from '../img/hero2.jpg';
+import hero3 from '../img/hero3.jpg';
+import hero4 from '../img/hero4.jpg';
+import hero5 from '../img/hero5.jpg';
+import hero6 from '../img/hero6.jpg';
 
 
 function Banner(props) {
@@ -13,9 +18,74 @@ function Banner(props) {
         />
         <Carousel.Caption>
           <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          
         </Carousel.Caption>
       </Carousel.Item>
+
+
+   <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero2}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+
+
+   <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero3}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+
+
+   <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero4}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero5}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+   
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero6}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+   
       
     </Carousel>
   );
